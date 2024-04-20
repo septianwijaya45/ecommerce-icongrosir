968cc3c736bd39e71ac3a7a7c1d63ba11492c78e
septianwijaya45 <septianwijaya170845@gmail.com>
2024-04-21 04:32:19 +0700

    adding template & setting

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">    
+    <title>Icon Grosir | Home</title>
+    
+    <!-- Font awesome -->
+    <link href="{{ asset('css/font-awesome.css') }}" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">   
+    <!-- SmartMenus jQuery Bootstrap Addon CSS -->
+    <link href="{{ asset('css/jquery.smartmenus.bootstrap.css') }}" rel="stylesheet">
+    <!-- Product view slider -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/jquery.simpleLens.css') }}">    
+    <!-- slick slider -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/slick.css') }}">
+    <!-- price picker slider -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/nouislider.css') }}">
+    <!-- Theme color -->
+    <link id="switcher" href="{{ asset('css/theme-color/default-theme.css') }}" rel="stylesheet">
+    <!-- <link id="switcher" href="css/theme-color/bridge-theme.css" rel="stylesheet"> -->
+    <!-- Top Slider CSS -->
+    <link href="{{ asset('css/sequence-theme.modern-slide-in.css') }}" rel="stylesheet" media="all">
+    <!-- Icon -->
+    <link rel="shortcut icon" href="{{ asset('img/logo/icongrosir.png') }}" type="image/x-icon">
+
+    <!-- Main style sheet -->
+    <link href="css/style.css" rel="stylesheet">    
+
+    <!-- Google Font -->
+    <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>  
+
+  </head>
+  <body>
+   <!-- wpf loader Two -->
+    <div id="wpf-loader-two">          
+      <div class="wpf-loader-two-inner">
+        <span>Loading</span>
+      </div>
+    </div> 
+    <!-- / wpf loader Two -->
+    <!-- SCROLL TOP BUTTON -->
+        <a class="scrollToTop" href="#"><i class="fa fa-chevron-up"></i></a>
+    <!-- END SCROLL TOP BUTTON -->
+    @include('layouts.partials.header')
+    @include('layouts.partials.menu')
+
+    @yield('content')
+
+  
+    <!-- Login Modal -->  
+    <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+        <div class="modal-content">                      
+            <div class="modal-body">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <h4>Login or Register</h4>
+            <form class="aa-login-form" action="">
+                <label for="">Username or Email address<span>*</span></label>
+                <input type="text" placeholder="Username or email">
+                <label for="">Password<span>*</span></label>
+                <input type="password" placeholder="Password">
+                <button class="aa-browse-btn" type="submit">Login</button>
+                <label for="rememberme" class="rememberme"><input type="checkbox" id="rememberme"> Remember me </label>
+                <p class="aa-lost-password"><a href="#">Lost your password?</a></p>
+                <div class="aa-register-now">
+                Don't have an account?<a href="account.html">Register now!</a>
+                </div>
+            </form>
+            </div>                        
+        </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div>    
+
+    @include('layouts.partials.footer')
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="{{ asset('js/bootstrap.js') }}"></script>  
+    <!-- SmartMenus jQuery plugin -->
+    <script type="text/javascript" src="{{ asset('js/jquery.smartmenus.js') }}"></script>
+    <!-- SmartMenus jQuery Bootstrap Addon -->
+    <script type="text/javascript" src="{{ asset('js/jquery.smartmenus.bootstrap.js') }}"></script>  
+    <!-- To Slider JS -->
+    <script src="{{ asset('js/sequence.js') }}"></script>
+    <script src="{{ asset('js/sequence-theme.modern-slide-in.js') }}"></script>  
+    <!-- Product view slider -->
+    <script type="text/javascript" src="{{ asset('js/jquery.simpleGallery.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/jquery.simpleLens.js') }}"></script>
+    <!-- slick slider -->
+    <script type="text/javascript" src="{{ asset('js/slick.js') }}"></script>
+    <!-- Price picker slider -->
+    <script type="text/javascript" src="{{ asset('js/nouislider.js') }}"></script>
+    <!-- Custom js -->
+    <script src="{{ asset('js/custom.js') }}"></script> 
+
+  </body>
+</html>
---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,132 @@
+ <!-- Start header section -->
+  <header id="aa-header">
+    <!-- start header top  -->
+    <div class="aa-header-top">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="aa-header-top-area">
+              <!-- start header top left -->
+              <div class="aa-header-top-left">
+                <!-- start language -->
+                <div class="aa-language">
+                  <div class="dropdown">
+                    <a class="btn dropdown-toggle" href="#" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                      <img src="img/flag/indonesia.png" alt="english flag">INDONESIA
+                      <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                      <li><a href="#"><img src="img/flag/indonesia.png" alt="">INDONESIA</a></li>
+                    </ul>
+                  </div>
+                </div>
+                <!-- / language -->
+
+                <!-- start currency -->
+                <div class="aa-currency">
+                  <div class="dropdown">
+                    <a class="btn dropdown-toggle" href="#" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                      <i class="fas fa-rupiah"></i>Rupiah
+                      <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                      <li><a href="#"><i class="fa fa-usd"></i>USD</li>
+                      <li><a href="#"><i class="fa fa-euro"></i>EURO</a></li>
+                    </ul>
+                  </div>
+                </div>
+                <!-- / currency -->
+                <!-- start cellphone -->
+                <div class="cellphone hidden-xs">
+                  <p><span class="fa fa-phone"></span>0857-0000-1111</p>
+                </div>
+                <!-- / cellphone -->
+              </div>
+              <!-- / header top left -->
+              <div class="aa-header-top-right">
+                <ul class="aa-head-top-nav-right">
+                  <li><a href="account.html">Akun Saya</a></li>
+                  <li class="hidden-xs"><a href="wishlist.html">Wishlist</a></li>
+                  <li class="hidden-xs"><a href="cart.html">My Cart</a></li>
+                  <li class="hidden-xs"><a href="checkout.html">Checkout</a></li>
+                  <li><a href="" data-toggle="modal" data-target="#login-modal">Login</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- / header top  -->
+
+    <!-- start header bottom  -->
+    <div class="aa-header-bottom">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="aa-header-bottom-area">
+              <!-- logo  -->
+              <div class="aa-logo">
+                <!-- Text based logo -->
+                <a href="index.html">
+                  <span class="fa fa-shopping-cart"></span>
+                  <p>Icon<strong>Grosir Shop</strong> <span>Your Shopping Partner</span></p>
+                </a>
+                <!-- img based logo -->
+                <!-- <a href="index.html"><img src="img/logo.jpg" alt="logo img"></a> -->
+              </div>
+              <!-- / logo  -->
+               <!-- cart box -->
+              <div class="aa-cartbox">
+                <a class="aa-cart-link" href="#">
+                  <span class="fa fa-shopping-basket"></span>
+                  <span class="aa-cart-title">SHOPPING CART</span>
+                  <span class="aa-cart-notify">2</span>
+                </a>
+                <div class="aa-cartbox-summary">
+                  <ul>
+                    <li>
+                      <a class="aa-cartbox-img" href="#"><img src="img/products/kaos3.png" alt="img"></a>
+                      <div class="aa-cartbox-info">
+                        <h4><a href="#">T-Shirt Men New Arrival Casual Short</a></h4>
+                        <p>200 Kaos</p>
+                      </div>
+                      <a class="aa-remove-product" href="#"><span class="fa fa-times"></span></a>
+                    </li>
+                    <li>
+                      <a class="aa-cartbox-img" href="#"><img src="img/products/kaos2.png" alt="img"></a>
+                      <div class="aa-cartbox-info">
+                        <h4><a href="#">T-Shirt Men Dark</a></h4>
+                        <p>300 Kaos</p>
+                      </div>
+                      <a class="aa-remove-product" href="#"><span class="fa fa-times"></span></a>
+                    </li>                    
+                    <li>
+                      <span class="aa-cartbox-total-title">
+                        Total
+                      </span>
+                      <span class="aa-cartbox-total-price">
+                        Rp 25.000.000,-
+                      </span>
+                    </li>
+                  </ul>
+                  <a class="aa-cartbox-checkout aa-primary-btn" href="checkout.html">Checkout</a>
+                </div>
+              </div>
+              <!-- / cart box -->
+              <!-- search box -->
+              <div class="aa-search-box">
+                <form action="">
+                  <input type="text" name="" id="" placeholder="Search here ex. 'man' ">
+                  <button type="submit"><span class="fa fa-search"></span></button>
+                </form>
+              </div>
+              <!-- / search box -->             
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- / header bottom  -->
+  </header>
+  <!-- / header section -->
