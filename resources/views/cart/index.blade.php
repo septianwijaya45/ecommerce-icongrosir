1d0884048279d35e7f04b48a7c03a39460053946
septianwijaya45 <septianwijaya170845@gmail.com>
2025-03-10 06:31:17 +0700

    fixing bug cart terhapus

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -19,6 +19,103 @@
         </div>
     </div>
 </section>
+<section id="cart-view">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="cart-view-area">
+            <div class="cart-view-table">
+              <form action="">
+                <div class="table-responsive">
+                   <table class="table">
+                     <thead>
+                       <tr>
+                          <th></th>
+                          <th>Produk</th>
+                          <th>Varian</th>
+                          <th>Warna</th>
+                          <th>Ukuran</th>
+                          <th>Harga Satuan</th>
+                          <th>Qty</th>
+                       </tr>
+                     </thead>
+                        <tbody id="list-product">
+                            @php
+                            $total = 0;
+                            @endphp
+                            @forelse($carts as $data)
+                            @php
+                            $total += $data['hargas']*$data['qty'];
+                            @endphp
+
+                                <tr>
+                                    <td><button type="button" class="btn btn-danger" id="btn-delete" onclick="deleteData(`{{ $data['id'] }}`)"><fa class="fa fa-trash"></fa></button></td>
+                                    <td>
+                                        <a href="#">
+                                            <img src="{{ !is_null($data['image']) ? $urlPhoto.$data['image'] : 'img/default/defaultProduct.png'}}" alt="img">
+                                        </a>
+                                        <br>
+                                        <p>{{ $data['nama_barang'] }}</p>
+                                    </td>
+                                    <td>
+                                    <select class="form-control" id="variant-{{ $data['id'] }}" onchange="loadVariant(`{{ $data['uuid'] }}`, this.value, `{{ $data['id'] }}`)" name="variant" readonly>
+                                        <option value="">Pilih Varian</option>
+                                        @foreach($data['variants'] as $variant)
+                                        <option value="{{ $variant['variasi_detail'] }}" @if($variant['variasi_detail'] == $data['varian']) selected @endif>{{ $variant['variasi_detail'] }}</option>
+                                        @endforeach
+                                    </select>
+                                    </td>
+                                    <td>
+                                    <select class="form-control" id="warna-{{ $data['id'] }}" onchange="loadColors(`{{ $data['uuid'] }}`, this.value, `{{ $data['id'] }}`)" name="warna" readonly>
+                                        <option value="">Pilih Warna</option>
+                                        @foreach($data['warnas'] as $warna)
+                                        <option value="{{ $warna['warna'] }}" @if($warna['warna'] == $data['warna']) selected @endif>{{ $warna['warna'] }}</option>
+                                        @endforeach
+                                    </select>
+                                    </td>
+                                    <td>
+                                    <select class="form-control" id="ukuran-{{ $data['id'] }}" onchange="loadSizes(`{{ $data['uuid'] }}`, this.value, `{{ $data['id'] }}`)" name="ukuran" readonly>
+                                        <option value="">Pilih Ukuran</option>
+                                        @foreach($data['ukurans'] as $ukuran)
+                                        <option value="{{ $ukuran['ukuran'] }}" @if($ukuran['ukuran'] == $data['ukuran']) selected @endif>{{ $ukuran['ukuran'] }}</option>
+                                        @endforeach
+                                    </select>
+                                    </td>
+                                    <td>{{ formatRupiah($data['hargas']) }}</td>
+                                    <td width="10%">
+                                        <input type="number" class="form-control" value="{{ $data['qty'] }}" placeholder="QTY Dipesan" onchange="changeQty(`{{ $data['uuid'] }}`, this.value, `{{ $data['id'] }}`)" oninput="changeQty(`{{ $data['uuid'] }}`, this.value, `{{ $data['id'] }}`)" id="qty-{{ $data['id'] }}" data-id="{{ $data['id'] }}" data-product="{{ $data['product_id'] }}" data-variant="{{ $data['variant_id'] }}">
+                                    </td>
+                                </tr>
+                                @empty
+                                <tr>
+                                    <td colspan="8" class="text-center text-danger">Tidak Ada Produk Pada Wishlist Anda</td>
+                                </tr>
+                                @endforelse
+                        </tbody>
+                   </table>
+                 </div>
+              </form>
+              <!-- Cart Total view -->
+              @if(count($carts) > 0)
+                <div class="cart-view-total">
+                    <h4>Total Biaya di Keranjang Anda</h4>
+                    <table class="aa-totals-table">
+                    <tbody>
+                        <tr>
+                        <th>Total</th>
+                        <td id="totalHarga">{{ formatRupiah($total) }}</td>
+                        </tr>
+                    </tbody>
+                    </table>
+                    <a href="{{ route('checkout.store') }}" class="aa-cart-view-btn">Proced to Checkout</a>
+                </div>
+              @endif
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
 @stop
 
 @section('script')
